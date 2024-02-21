fe7e7f7573ffda82631b262f191702c8eb8e5577
DanielJensenKEA <daje0003@stud.kea.dk>
2024-02-21 12:38:00 +0100

    Version 3 - Arrayliste og While loop

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -1,16 +1,31 @@
+import java.util.ArrayList;
+
 public class MovieCollection {
-    private Movie [] filmSamling;
-    int count = 0;
+   // private Movie [] filmSamling;
+    //int count = 0;
+    ArrayList<Movie> movieList;
+
 
     public MovieCollection() {
-        this.filmSamling = new Movie[5];
+        //this.filmSamling = new Movie[5];
+        movieList = new ArrayList<>();
+
+
     }
 
     public void addMovie(String title, String director, int yearCreated, boolean isInColor, int lengthInMinutes, String genre) {
-        filmSamling[count++] = new Movie(title, director, yearCreated, isInColor, lengthInMinutes, genre);
+        movieList.add(new Movie(title, director, yearCreated, isInColor, lengthInMinutes, genre));
+        //Bekræftelse på at det virker.
+        System.out.println(movieList.get(movieList.size() -1).getTitle()+" er blevet tilføjet til listen.");
+
     }
 
+
+
+/*
     public Movie[] getFilmSamling() {
         return filmSamling;
     }
+
+ */
 }
--- src/main.java
+++ src/main.java
@@ -1,29 +1,68 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        MovieCollection o = new MovieCollection();
+        //Vi kommenterer Moviecollection ud og tilføjer vores controller klasse.
+        //MovieCollection o = new MovieCollection();
+        Controller o = new Controller();
+
+        //Start på While Loop, menu til bruger.
+        final int SENTINEL = 2;
+        int loopInput = 1;
+
+        while (loopInput != SENTINEL ) {
+            System.out.println("Velkommen til din filmsamling.");
+            System.out.println("Tast 1 for at oprette en film");
+            System.out.println("Tast 2 for at afslutte");
+            loopInput = input.nextInt();
+            input.nextLine();
+
+            if(loopInput == 1) {
+                //I nedenstående kode er der indsat input.nextLine efter hver int eller boolean variabel i et
+                // forsøg på at redde programmet fra Scanner bug.
+
+                System.out.println("Skriv titlen på filmen.");
+                String title = input.nextLine();
+
+
+
+                System.out.println("Skriv navnet på instruktøren.");
+                String director = input.nextLine();
+                System.out.println("Skriv årstallet på, hvornår filmen blev udgivet.");
+                int yearCreated = input.nextInt();
+                input.nextLine();
+
+                System.out.println("Skriv, hvorvidt filmen er i farve eller ej. True/False.");
+                boolean isInColor = input.nextBoolean();
+                input.nextLine();
+
+                System.out.println("Skriv varigheden af filmen i minutter.");
+                int lengthInMinutes = input.nextInt();
+                input.nextLine();
+
+                System.out.println("Skriv genren.");
+                String genre = input.next();
+
+                o.addMovie(title, director, yearCreated, isInColor, lengthInMinutes, genre);
+
+
+
+                //Kode er udkommenteret.
+               // Movie[] test2 = o.getFilmSamling();
+                //System.out.println(test2[0].getTitle() + "er tilføjet til listen.");
+            }
+            else {
+                System.out.println("Du har afsluttet programmet.");
+            }
+
+
+
+        }
 
         //Bruger input til constructor
 
-        System.out.println("Skriv titlen på filmen.");
-        String title = input.nextLine();
 
-        System.out.println("Skriv navnet på instruktøren.");
-        String director = input.nextLine();
-        System.out.println("Skriv årstallet på, hvornår filmen blev udgivet.");
-        int yearCreated = input.nextInt();
-        System.out.println("Skriv, hvorvidt filmen er i farve eller ej. True/False.");
-        boolean isInColor = input.nextBoolean();
-        System.out.println("Skriv varigheden af filmen i minutter.");
-        int lengthInMinutes = input.nextInt();
-        System.out.println("Skriv genren.");
-        String genre = input.next();
-
-        o.addMovie(title, director, yearCreated, isInColor, lengthInMinutes, genre );
-
-        Movie[] test2 = o.getFilmSamling();
-        System.out.println(test2[0].getTitle() + "er tilføjet til listen.");
     }
 }
